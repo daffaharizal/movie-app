72a27adde4228027e41b5a794f073fadd406863c
Daffa Harizal Fitri <harizal.daffa46@gmail.com>
2024-05-29 23:44:53 +0700

    feat: create reusable component with standalone component

---
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-breadcrumb',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './breadcrumb.component.html',
+  styleUrls: ['./breadcrumb.component.scss']
+})
+export class BreadcrumbComponent {
+  @Input({ required: true }) namePreviousPage!: string;
+  @Input({ required: true }) nameCurrentPage!: string;
+
+  private router = inject(Router);
+  
+  previousPage() {
+    this.router.navigate(['/']);
+  }
+}
